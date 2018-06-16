8cf18367e667dc8865bac8e1ee8823a869e83e14
ihoney <102505481@qq.com>
2018-06-16 16:54:49 +0800

    修复https站扫描

---
+++ README.md
@@ -0,0 +1,119 @@
+# ihoneyBakFileScan v0.1 一个网站备份文件泄露扫描工具
+[![Python3](https://img.shields.io/badge/python-3.5-green.svg?style=plastic)](https://www.python.org/)
+
+## 1. 简介
+
+##### 1.1 网站备份文件泄露可能造成的危害：
+```
+1. 网站存在备份文件：网站存在备份文件，例如数据库备份文件、网站源码备份文件等，攻击者利用该信息可以更容易得到网站权限，导致网站被黑。
+2. 敏感文件泄露是高危漏洞之一，敏感文件包括数据库配置信息，网站后台路径，物理路径泄露等，此漏洞可以帮助攻击者进一步攻击，敞开系统的大门。
+3. 由于目标备份文件较大（xxx.G），可能存在更多敏感数据泄露
+4. 该备份文件被下载后，可以被用来做代码审计，进而造成更大的危害
+5. 该信息泄露会暴露服务器的敏感信息，使攻击者能够通过泄露的信息进行进一步入侵。
+```
+
+##### 1.2 工具核心：
+```
+1. 常见后缀：
+   * '.rar', '.sql', '.zip', '.gz', '.sql.gz', '.tar.gz', '.bak', '.sql.bak' ...
+2. 文件头识别:
+   * rar:526172211a0700cf9073
+   * zip:504b0304140000000800
+   * gz：1f8b080000000000000b，也包括'.sql.gz'，'.tar.gz'
+   * sql：每种导出方式有不同的文件头
+       * Adminer：  
+       * mysqldump：     
+       * phpMyAdmin：
+       * navicat：   
+3. 数据库备份导出方式识别：
+   * 导出方式                      文件头字符:                    前10个16进制字符：
+   * mysqldump:                   -- MySQL dump:               2d2d204d7953514c
+   * phpMyAdmin:                  -- phpMyAdmin SQL Dump:      2d2d207068704d794164
+   * navicat:                     /* Navicat :                 2f2a0a204e617669636174
+   * Adminer:                     -- Adminer x.x.x MySQL dump: 2d2d2041646d696e6572  (5月9日新增xxx.sql)
+   * Navicat MySQL Data Transfer: /* Navicat:                  2f2a0a4e617669636174
+   * 一种未知导出方式:               -- -------:                  2d2d202d2d2d2d2d2d2d
+4. 根据域名自动生成相关扫描字典:
+    ➜  ihoneyBakFileScan python3.5 ihoneyBakFileScan.py -u https://www.ihoney.net.cn
+    Queue size：49
+    [ ] https://www.ihoney.net.cn/__zep__/js.zip
+    [*] https://www.ihoney.net.cn/ihoney.bak  size:0M
+    [ ] https://www.ihoney.net.cn/ihoney.gz
+    [ ] https://www.ihoney.net.cn/ihoney.rar
+    [*] https://www.ihoney.net.cn/ihoney.sql  size:0M
+    [*] https://www.ihoney.net.cn/ihoney.sql.bak  size:0M
+    [ ] https://www.ihoney.net.cn/ihoney.sql.gz
+    [ ] https://www.ihoney.net.cn/ihoney.tar.gz
+    [ ] https://www.ihoney.net.cn/ihoney.zip
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.bak
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.gz
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.rar
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.sql
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.sql.bak
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.sql.gz
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.tar.gz
+    [ ] https://www.ihoney.net.cn/ihoney.net.cn.zip
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.bak
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.gz
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.rar
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.sql
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.sql.bak
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.sql.gz
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.tar.gz
+    [ ] https://www.ihoney.net.cn/ihoneynetcn.zip
+    [ ] https://www.ihoney.net.cn/www.bak
+    [ ] https://www.ihoney.net.cn/www.gz
+    [ ] https://www.ihoney.net.cn/www.rar
+    [ ] https://www.ihoney.net.cn/www.sql
+    [ ] https://www.ihoney.net.cn/www.sql.bak
+    [ ] https://www.ihoney.net.cn/www.sql.gz
+    [ ] https://www.ihoney.net.cn/www.tar.gz
+    [ ] https://www.ihoney.net.cn/www.zip
+    [ ] https://www.ihoney.net.cn/www.ihoney.net.cn.bak
+    [ ] https://www.ihoney.net.cn/www.ihoney.net.cn.gz
+    [ ] https://www.ihoney.net.cn/www.ihoney.net.cn.rar
+    [ ] https://www.ihoney.net.cn/www.ihoney.net.cn.sql
+    [ ] https://www.ihoney.net.cn/www.ihoney.net.cn.sql.bak
+    [ ] https://www.ihoney.net.cn/www.ihoney.net.cn.sql.gz
+    [ ] https://www.ihoney.net.cn/www.ihoney.net.cn.tar.gz
+    [*] https://www.ihoney.net.cn/www.ihoney.net.cn.zip  size:0M
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.bak
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.gz
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.rar
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.sql
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.sql.bak
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.sql.gz
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.tar.gz
+    [ ] https://www.ihoney.net.cn/wwwihoneynetcn.zip
+5. 自动记录扫描成功的备份地址到以时间命名的文件
+    例如 20180616_16-28-14.txt：
+    https://www.ihoney.net.cn/ihoney.sql  size:0M
+    https://www.ihoney.net.cn/www.ihoney.net.cn.zip  size:0M
+    https://www.ihoney.net.cn/ihoney.bak  size:0M
+    https://www.ihoney.net.cn/ihoney.sql.bak  size:0M
+
+```
+
+## 2. 使用方式
+```
+参数：
+    -h --help      查看工具使用帮助
+    -f --url-file  批量时指定存放url的文件,每行url需要指定http://或者https://，否则默认使用http://
+    -t --thread    指定线程数，建议100
+    -u --url       单个url扫描时指定url
+    -d --dict-file 自定义扫描字典
+使用:
+    批量url扫描    python3.5 ihoneyBakFileScan.py -t 100 -f url.txt
+    单个url扫描    python3.5 ihoneyBakFileScan.py -u https://www.ihoneysec.top/
+                  python3.5 ihoneyBakFileScan.py -u www.ihoney.net.cn
+                  python3.5 ihoneyBakFileScan.py -u www.ihoney.net.cn -d dict.txt
+```
+
+## 3. ChangeLog:
+* 2018.04.20  首发T00ls：支持rar,zip后缀备份文件头识别，根据域名自动生成相关扫描字典，自动记录扫描成功的备份地址到文件
+* 2018.04.26
+              在原本扫描成功的备份地址后增加了备份大小，以方便快速识别有效备份。
+              增加了.sql文件识别，也是识别文件头的方式，文件头我目前检测到三种，分别是不同方式导出的：1.mysql，2.phpmyadmin，3.navicat。
+* 2018.05.19  新增识别Adminer导出的两种格式：baidu.sql、baodu.sql.gz
+* 2018.05.31  新增Navicat MySQL Data Transfer备份导出方式和另一种未知导出方式
+* 2018.06.16  修复支持https站扫描，并从旧项目中抽出来独立作为一个项目
---
+++ ihoneyBakFileScan.py
@@ -0,0 +1,154 @@
+# 2018.04.20 www.T00ls.net
+# __author__: ihoneysec
+import requests
+import logging
+from binascii import b2a_hex
+from queue import Queue
+from threading import Thread
+from argparse import ArgumentParser
+from copy import deepcopy
+from datetime import datetime
+
+requests.packages.urllib3.disable_warnings()
+
+logging.basicConfig(level=logging.WARNING, format="%(message)s")
+
+
+class BakScan(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+        self.headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36", }
+        self.timeout = 5  # The 3 seconds test is missing
+
+    def _auth(self, url):
+        try:
+            r = requests.get(url=url, headers=self.headers, timeout=self.timeout, allow_redirects=False, stream=True, verify=False)
+
+            content = b2a_hex(r.raw.read(10)).decode()
+            if r.status_code == 200:
+                rarsize = int(r.headers.get('Content-Length')) // 1024 // 1024
+                if content.startswith('526172') or content.startswith('504b03') or content.startswith('1f8b080000000000000b') or content.startswith('2d2d204d7953514c') or content.startswith(
+                        '2d2d207068704d794164') or content.startswith('2f2a0a204e6176696361') or content.startswith('2d2d2041646d696e6572') or content.startswith(
+                    '2d2d202d2d2d2d2d2d2d') or content.startswith('2f2a0a4e617669636174') or url.endswith('.sql') or url.endswith('.sql.bak') or url.endswith('.bak'):
+                    logging.warning('[*] {}  size:{}M'.format(url, rarsize))
+                    with open(datefile, 'a') as f:
+                        try:
+                            f.write(str(url) + '  ' + 'size:' + str(rarsize) + 'M' + '\n')
+                        except:
+                            pass
+                else:
+                    logging.warning('[ ] {}'.format(url))
+            else:
+                logging.warning('[ ] {}'.format(url))
+        except Exception as e:
+            pass
+
+    def run(self):
+        while not self.queue.empty():
+            url = self.queue.get()
+            try:
+                self._auth(url)
+            except:
+                continue
+
+
+def urlcheck(target=None, ulist=None):
+    if target is not None and ulist is not None:
+        if target.startswith('http://') or target.startswith('https://'):
+            if target.endswith('/'):
+                ulist.append(target)
+            else:
+                ulist.append(target + '/')
+        else:
+            line = 'http://' + target
+            if line.endswith('/'):
+                ulist.append(line)
+            else:
+                ulist.append(line + '/')
+        return ulist
+
+
+def dispatcher(url_file=None, url=None, max_thread=1, dic=None):
+    urllist = []
+
+    if url_file is not None and url is None:
+        with open(str(url_file)) as f:
+            while True:
+                line = str(f.readline()).strip()
+                if line:
+                    urllist = urlcheck(line, urllist)
+                else:
+                    break
+    elif url is not None and url_file is None:
+        url = str(url.strip())
+        urllist = urlcheck(url, urllist)
+    else:
+        pass
+
+    with open(datefile, 'w'):
+        pass
+
+    q = Queue()
+
+    for u in urllist:
+        ucp = u.strip('http://').strip('https://').strip('/')
+        www1 = ucp.split('.')
+        wwwlen = len(www1)
+        wwwhost = ''
+        for i in range(1, wwwlen):
+            wwwhost += www1[i]
+
+        current_info_dic = deepcopy(dic)  # deep copy
+        suffixFormat = ['.rar', '.sql', '.zip', '.gz', '.sql.gz', '.tar.gz', '.bak', '.sql.bak']
+        domainDic = [ucp, ucp.replace('.', ''), wwwhost, ucp.split('.', 1)[-1], www1[0], www1[1]]
+
+        for s in suffixFormat:
+            for d in domainDic:
+                current_info_dic.extend([d + s])
+
+        for info in current_info_dic:
+            url = str(u) + str(info)
+            q.put(url)
+
+    # print(q.__dict__['queue'])
+    print('Queue size：' + str(q.qsize()))
+    threadl = [BakScan(q) for _ in range(max_thread)]
+    for t in threadl:
+        t.start()
+
+    for t in threadl:
+        t.join()
+
+
+if __name__ == '__main__':
+    usageexample = '\n       Example: python3.5 ihoneyBakFileScan -t 100 -f url.txt\n'
+    usageexample += '                '
+    usageexample += 'python3.5 ihoneyBakFileScan.py -u https://www.example.com/'
+
+    parser = ArgumentParser(add_help=True, usage=usageexample, description='A Website Backup File Leak Scan Tool.')
+    parser.add_argument('-f', '--url-file', dest="url_file", help="Example: url.txt")
+    parser.add_argument('-t', '--thread', dest="max_threads", nargs='?', type=int, default=1, help="Max threads")
+    parser.add_argument('-u', '--url', dest='url', nargs='?', type=str, help="Example: http://www.example.com/")
+    parser.add_argument('-d', '--dict-file', dest='dict_file', nargs='?', help="Example: dict.txt")
+
+    args = parser.parse_args()
+    # Use the program default dictionary，Accurate scanning mode，Automatic dictionary generation based on domain name.
+    info_dic = ['__zep__/js.zip', ]
+
+    datefile = datetime.now().strftime('%Y%m%d_%H-%M-%S.txt')
+
+    try:
+        if args.dict_file:
+            # Custom scan dictionary
+            # This mode is not recommended for bulk scans. It is prone to false positives and can reduce program efficiency.
+            custom_dict = list(set([i.replace("\n", "") for i in open(str(args.dict_file), "r").readlines()]))
+            info_dic = info_dic.extend(custom_dict)
+        if args.url:
+            dispatcher(url=args.url, max_thread=args.max_threads, dic=info_dic)
+        elif args.url_file:
+            dispatcher(url_file=args.url_file, max_thread=args.max_threads, dic=info_dic)
+        else:
+            print("[!] Please specify a URL, or URL file name.")
+    except Exception as e:
+        pass
